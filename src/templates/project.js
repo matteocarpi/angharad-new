332403090c0ce0e745dde199f4d6c88c6db50bde
Matteo Carpi <matteocarpi94@gmail.com>
2021-04-14 13:50:02 +0200

    Fixing deploy

--- src/templates/project.js
+++ src/templates/project.js
@@ -1,46 +1,55 @@
-import React, { useState } from 'react';
-import Layout from '../components/layout';
-import styles from '../styles/Project.module.scss';
-import { graphql } from 'gatsby';
-import { PropTypes } from 'prop-types';
-import Markdown from 'markdown-to-jsx';
-import Modali, { useModali } from 'modali';
-import { FaChevronRight, FaChevronLeft } from 'react-icons/fa';
-import classnames from 'classnames';
-import ReactPlayer from 'react-player';
-import Img from 'gatsby-image';
+import React, { useState } from "react"
+import Layout from "../components/layout"
+import styles from "../styles/Project.module.scss"
+import { graphql } from "gatsby"
+import { PropTypes } from "prop-types"
+import Markdown from "markdown-to-jsx"
+import Modali, { useModali } from "modali"
+import { FaChevronRight, FaChevronLeft } from "react-icons/fa"
+import classnames from "classnames"
+import ReactPlayer from "react-player"
+import Img from "gatsby-image"
 
 const Project = ({ data }) => {
-  const images = data.postData.frontmatter.gallery;
-  let [selectedImage, setSelectedImage] = useState(0);
-  const [lightbox, toggleLightbox] = useModali({ centered: true, large:true });
-  
+  const images = data.postData.frontmatter.gallery
+  let [selectedImage, setSelectedImage] = useState(0)
+  const [lightbox, toggleLightbox] = useModali({ centered: true, large: true })
+
   return (
     <Layout>
       <div className={styles.project}>
-        <Modali.Modal
-          {...lightbox}
-        >
-          <div
-            className={styles.lightboxContainer}
-          >
-            <FaChevronLeft className={styles.navIcon} onClick={() => setSelectedImage(selectedImage - 1)}/>
+        <Modali.Modal {...lightbox}>
+          <div className={styles.lightboxContainer}>
+            <FaChevronLeft
+              className={styles.navIcon}
+              onClick={() => setSelectedImage(selectedImage - 1)}
+            />
 
-            {images && <Img className={styles.image} fluid={images[selectedImage] && images[selectedImage].childImageSharp && images[selectedImage].childImageSharp.fluid}/>
-            }
-          
+            {images && (
+              <Img
+                className={styles.image}
+                fluid={
+                  images[selectedImage] &&
+                  images[selectedImage].childImageSharp &&
+                  images[selectedImage].childImageSharp.fluid
+                }
+              />
+            )}
+
             <FaChevronRight
               className={styles.navIcon}
               onClick={() => {
-                selectedImage === images.length - 1 ?
-                  setSelectedImage(0)
-                  :
-                  setSelectedImage(selectedImage + 1);
-              }}/>
+                selectedImage === images.length - 1
+                  ? setSelectedImage(0)
+                  : setSelectedImage(selectedImage + 1)
+              }}
+            />
           </div>
         </Modali.Modal>
 
-        <h1 className={styles.mobileTitle}>{data.postData.frontmatter.title}</h1>
+        <h1 className={styles.mobileTitle}>
+          {data.postData.frontmatter.title}
+        </h1>
         <section className={styles.visual}>
           {data.postData.frontmatter.video && (
             <div className={styles.videoWrap}>
@@ -51,65 +60,83 @@ const Project = ({ data }) => {
               />
             </div>
           )}
-          {images && images.map((image, index) => {
-            const square = {
-              ...image.childImageSharp.fluid, 
-              aspectRatio: 1 / 1,
-            };
-            const rectangle = {
-              ...image.childImageSharp.fluid, aspectRatio: 16 / 9,
-            };
-            return (
-              <button
-                key={image}
-                onClick={() => {
-                  toggleLightbox();
-                  setSelectedImage(index);
-                }}
-                className={classnames(styles.thumbWrap, !data.postData.frontmatter.video && index === 0 && styles.first)}
-              >
-              
-                <Img className={classnames(styles.thumb, !data.postData.frontmatter.video && index === 0 && styles.first)} sizes={!data.postData.frontmatter.video && index === 0 ? rectangle : square} />
-
-              </button>
-            );
-          })}
+          {images &&
+            images.map((image, index) => {
+              const square = {
+                ...(image && image.childImageSharp.fluid),
+                aspectRatio: 1 / 1,
+              }
+              const rectangle = {
+                ...(image && image.childImageSharp.fluid),
+                aspectRatio: 16 / 9,
+              }
+              return (
+                <button
+                  key={image}
+                  onClick={() => {
+                    toggleLightbox()
+                    setSelectedImage(index)
+                  }}
+                  className={classnames(
+                    styles.thumbWrap,
+                    !data.postData.frontmatter.video &&
+                      index === 0 &&
+                      styles.first
+                  )}
+                >
+                  <Img
+                    className={classnames(
+                      styles.thumb,
+                      !data.postData.frontmatter.video &&
+                        index === 0 &&
+                        styles.first
+                    )}
+                    sizes={
+                      !data.postData.frontmatter.video && index === 0
+                        ? rectangle
+                        : square
+                    }
+                  />
+                </button>
+              )
+            })}
         </section>
 
         <section className={styles.reading}>
-          <h1 className={styles.desktopTitle}>{data.postData.frontmatter.title}</h1>
+          <h1 className={styles.desktopTitle}>
+            {data.postData.frontmatter.title}
+          </h1>
           <Markdown>{data.postData.internal.content}</Markdown>
         </section>
-
       </div>
     </Layout>
-  );
-};
+  )
+}
 
-export default Project;
+export default Project
 
 export const query = graphql`
-query PostData($slug: String!) {
-  postData: markdownRemark(fields: {slug: {eq: $slug}}) {
-        frontmatter {
-          title
-          gallery {
-            childImageSharp {
-              fluid(maxWidth: 1024) {
-                ...GatsbyImageSharpFluid
-              }
+  query PostData($slug: String!) {
+    postData: markdownRemark(fields: { slug: { eq: $slug } }) {
+      frontmatter {
+        title
+        gallery {
+          childImageSharp {
+            fluid(maxWidth: 1024) {
+              ...GatsbyImageSharpFluid
             }
           }
-          video
-          videoLink
         }
-        internal {
-          content
-        }
+        video
+        videoLink
+      }
+      internal {
+        content
+      }
     }
-}
-`;
+  }
+`
 
 Project.propTypes = {
   data: PropTypes.node,
-};
+}
